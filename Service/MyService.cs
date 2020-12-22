16d0348922fd233e35e1f701a9e4b07f3c8576d5
Ramon Smits <ramon.smits@gmail.com>
2020-12-22 13:42:50 +0100

    Added some console diagnostics output

--- Service/MyService.cs
+++ Service/MyService.cs
@@ -30,51 +30,73 @@ namespace Service
             await base.StopAsync(cancellationToken);
         }
 
+        static int count;
+
         protected override async Task ExecuteAsync(CancellationToken token)
         {
-            const string EndpointName = "endpoint name";
-            var endpointConfiguration = new EndpointConfiguration(EndpointName);
-            endpointConfiguration.UniquelyIdentifyRunningInstance().UsingNames(EndpointName, Environment.MachineName);
+            var line = new string('=', 80);
 
-
-            endpointConfiguration.SendFailedMessagesTo("error");
-            endpointConfiguration.AuditProcessedMessagesTo("audit");
-
-            endpointConfiguration.DefineCriticalErrorAction(OnCriticalError);
-
-            ConventionsBuilder conventions = endpointConfiguration.Conventions();
-            conventions
-                .DefiningEventsAs(_ => _.Name.EndsWith("Event"))
-                .DefiningCommandsAs(_ => _.Name.EndsWith("Command"))
-                .DefiningMessagesAs(_ => _.Name.EndsWith("Message"));
-
-            TransportExtensions<SqlServerTransport> transport =
-                endpointConfiguration.UseTransport<SqlServerTransport>();
-
-            NServiceBus.Transport.SQLServer.SubscriptionSettings
-                subscriptionSettings = transport.SubscriptionSettings();
-            subscriptionSettings.DisableSubscriptionCache();
-            subscriptionSettings.SubscriptionTableName("subscriptions", "transportSchema");
-
-            transport.Routing().RouteToEndpoint(
-                Assembly.GetAssembly(typeof(MyCommand)),
-                EndpointName);
-
-
-            DelayedDeliverySettings delayedDeliverySettings = transport.NativeDelayedDelivery();
-            delayedDeliverySettings.DisableTimeoutManagerCompatibility();
-
-            transport
-                .Transactions(TransportTransactionMode.ReceiveOnly)
-                .ConnectionString("Data Source=sqlserver;Initial Catalog=transport;User ID=worker;Password=password;Max Pool Size=100;")
-                .TransactionScopeOptions(TimeSpan.FromSeconds(30), IsolationLevel.ReadCommitted)
-                .DefaultSchema("transportSchema");
-
-            this.endpoint = await Endpoint.Start(endpointConfiguration);
-
-            while (!token.IsCancellationRequested)
+            try
             {
-                await Task.Delay(1000, token);
+                const string EndpointName = "endpoint name";
+                var endpointConfiguration = new EndpointConfiguration(EndpointName);
+                endpointConfiguration.UniquelyIdentifyRunningInstance()
+                    .UsingNames(EndpointName, Environment.MachineName);
+
+
+                endpointConfiguration.SendFailedMessagesTo("error");
+                endpointConfiguration.AuditProcessedMessagesTo("audit");
+
+                endpointConfiguration.DefineCriticalErrorAction(OnCriticalError);
+
+                ConventionsBuilder conventions = endpointConfiguration.Conventions();
+                conventions
+                    .DefiningEventsAs(_ => _.Name.EndsWith("Event"))
+                    .DefiningCommandsAs(_ => _.Name.EndsWith("Command"))
+                    .DefiningMessagesAs(_ => _.Name.EndsWith("Message"));
+
+                TransportExtensions<SqlServerTransport> transport =
+                    endpointConfiguration.UseTransport<SqlServerTransport>();
+
+                NServiceBus.Transport.SQLServer.SubscriptionSettings
+                    subscriptionSettings = transport.SubscriptionSettings();
+                subscriptionSettings.DisableSubscriptionCache();
+                subscriptionSettings.SubscriptionTableName("subscriptions", "transportSchema");
+
+                transport.Routing().RouteToEndpoint(
+                    Assembly.GetAssembly(typeof(MyCommand)),
+                    EndpointName);
+
+
+                DelayedDeliverySettings delayedDeliverySettings = transport.NativeDelayedDelivery();
+                delayedDeliverySettings.DisableTimeoutManagerCompatibility();
+
+                transport
+                    .Transactions(TransportTransactionMode.ReceiveOnly)
+                    .ConnectionString(
+                        "Data Source=.;Database=SqlServerSimple;Integrated Security=True;Max Pool Size=100")
+                    .TransactionScopeOptions(TimeSpan.FromSeconds(30), IsolationLevel.ReadCommitted)
+                    .DefaultSchema("transportSchema");
+
+                count++;
+                Console.WriteLine(line);
+                Console.WriteLine(count + " Starting");
+                this.endpoint = await Endpoint.Start(endpointConfiguration);
+                Console.WriteLine(line);
+                Console.WriteLine(count + "Started");
+
+                while (!token.IsCancellationRequested)
+                {
+                    Console.WriteLine(line);
+                    Console.WriteLine(count + "Delay");
+                    await Task.Delay(1000, token);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(line);
+                Console.WriteLine(count + " Error!!!! " + e);
+                throw;
             }
         }
 
