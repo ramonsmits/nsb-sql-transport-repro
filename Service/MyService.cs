15a248f89b724e23dc15edbb345d19058fb8abaf
Philipp Dolder <philipp.dolder@mmssolutions.io>
2020-12-23 13:21:44 +0100

    Failfast when an exception occurs during service start. Exits with a code other than 0

--- Service/MyService.cs
+++ Service/MyService.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using System.Transactions;
 using Microsoft.Extensions.Hosting;
 using NServiceBus;
-using NServiceBus.Transport.SqlServer;
 
 namespace Service
 {
@@ -32,45 +31,54 @@ namespace Service
 
         protected override async Task ExecuteAsync(CancellationToken token)
         {
-            const string EndpointName = "endpoint name";
-            var endpointConfiguration = new EndpointConfiguration(EndpointName);
-            endpointConfiguration.UniquelyIdentifyRunningInstance().UsingNames(EndpointName, Environment.MachineName);
-
-
-            endpointConfiguration.SendFailedMessagesTo("error");
-            endpointConfiguration.AuditProcessedMessagesTo("audit");
-
-            endpointConfiguration.DefineCriticalErrorAction(OnCriticalError);
-
-            ConventionsBuilder conventions = endpointConfiguration.Conventions();
-            conventions
-                .DefiningEventsAs(_ => _.Name.EndsWith("Event"))
-                .DefiningCommandsAs(_ => _.Name.EndsWith("Command"))
-                .DefiningMessagesAs(_ => _.Name.EndsWith("Message"));
-
-            TransportExtensions<SqlServerTransport> transport =
-                endpointConfiguration.UseTransport<SqlServerTransport>();
-
-            NServiceBus.Transport.SqlServer.SubscriptionSettings
-                subscriptionSettings = transport.SubscriptionSettings();
-            subscriptionSettings.DisableSubscriptionCache();
-            subscriptionSettings.SubscriptionTableName("subscriptions", "transportSchema");
-
-            transport.Routing().RouteToEndpoint(
-                Assembly.GetAssembly(typeof(MyCommand)),
-                EndpointName);
-
-            transport
-                .Transactions(TransportTransactionMode.ReceiveOnly)
-                .ConnectionString("Data Source=sqlserver;Initial Catalog=transport;User ID=worker;Password=password;Max Pool Size=100;")
-                .TransactionScopeOptions(TimeSpan.FromSeconds(30), IsolationLevel.ReadCommitted)
-                .DefaultSchema("transportSchema");
-
-            this.endpoint = await Endpoint.Start(endpointConfiguration);
-
-            while (!token.IsCancellationRequested)
+            try
             {
-                await Task.Delay(1000, token);
+                const string EndpointName = "endpoint name";
+                var endpointConfiguration = new EndpointConfiguration(EndpointName);
+                endpointConfiguration.UniquelyIdentifyRunningInstance()
+                    .UsingNames(EndpointName, Environment.MachineName);
+
+
+                endpointConfiguration.SendFailedMessagesTo("error");
+                endpointConfiguration.AuditProcessedMessagesTo("audit");
+
+                endpointConfiguration.DefineCriticalErrorAction(OnCriticalError);
+
+                ConventionsBuilder conventions = endpointConfiguration.Conventions();
+                conventions
+                    .DefiningEventsAs(_ => _.Name.EndsWith("Event"))
+                    .DefiningCommandsAs(_ => _.Name.EndsWith("Command"))
+                    .DefiningMessagesAs(_ => _.Name.EndsWith("Message"));
+
+                TransportExtensions<SqlServerTransport> transport =
+                    endpointConfiguration.UseTransport<SqlServerTransport>();
+
+                NServiceBus.Transport.SqlServer.SubscriptionSettings
+                    subscriptionSettings = transport.SubscriptionSettings();
+                subscriptionSettings.DisableSubscriptionCache();
+                subscriptionSettings.SubscriptionTableName("subscriptions", "transportSchema");
+
+                transport.Routing().RouteToEndpoint(
+                    Assembly.GetAssembly(typeof(MyCommand)),
+                    EndpointName);
+
+                transport
+                    .Transactions(TransportTransactionMode.ReceiveOnly)
+                    .ConnectionString(
+                        "Data Source=sqlserver;Initial Catalog=transport;User ID=worker;Password=password;Max Pool Size=100;")
+                    .TransactionScopeOptions(TimeSpan.FromSeconds(30), IsolationLevel.ReadCommitted)
+                    .DefaultSchema("transportSchema");
+
+                this.endpoint = await Endpoint.Start(endpointConfiguration);
+
+                while (!token.IsCancellationRequested)
+                {
+                    await Task.Delay(1000, token);
+                }
+            }
+            catch (Exception e)
+            {
+                FailFast("Could not start service", e);
             }
         }
 
